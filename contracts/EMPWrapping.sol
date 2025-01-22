6a4f37e16fb87cd38db5f2992b67f4ab7347543f
harpreet singh <happybiostockcode07040@gmail.com>
2025-01-22 17:03:47 +0530

    contract changes

--- contracts/EMPWrapping.sol
+++ contracts/EMPWrapping.sol
@@ -1,58 +1,83 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-/// @title DOLA Token Contract
-/// @notice A stablecoin pegged to 1 USD, backed by BDOLA as collateral.
-contract DOLAToken is ERC20 {
-    IERC20 public bdolaToken; // BDOLA token used as collateral
-    uint256 public collateralizationRatio = 150; // 150% collateralization
+/// @title EMP Wrapping Contract
+/// @author YourName
+/// @notice Allows wrapping and unwrapping of EMP tokens into WEMP tokens
+contract EMPWrapping is ERC20 {
+    using SafeERC20 for IERC20;
+    address public Owner;
 
-    constructor(address _bdolaToken) ERC20("DOLA", "DOLA") {
-        bdolaToken = IERC20(_bdolaToken);
+    IERC20 public immutable EMP;
+
+    // Event to track wrapping actions
+    event Wrapped(address indexed user, uint256 amount);
+    // Event to track unwrapping actions
+    event Unwrapped(address indexed user, uint256 amount);
+
+    /// @notice Constructor to initialize the EMP token and WEMP details
+    /// @param _empAddress The address of the EMP token contract
+    constructor(address _empAddress) ERC20("Wrapped EMP", "WEMP") {
+        require(_empAddress != address(0), "Invalid EMP token address");
+        Owner = msg.sender;
+        EMP = IERC20(_empAddress);
     }
 
-    /// @notice Calculates the required BDOLA collateral for a given DOLA amount
-    /// @param dolaAmount The amount of DOLA to mint
-    /// @return The required collateral in BDOLA (1.5x DOLA amount for 150% collateralization)
-    function calculateCollateral(
-        uint256 dolaAmount
-    ) public view returns (uint256) {
-        return (dolaAmount * collateralizationRatio) / 100;
+    /// @notice Wrap EMP tokens into WEMP tokens
+    /// @param amount The amount of EMP tokens to wrap
+    function wrap(uint256 amount) external {
+        require(amount > 0, "Amount must be greater than 0");
+
+        // Ensure the user has approved the contract to transfer EMP tokens
+        uint256 allowance = EMP.allowance(msg.sender, address(this));
+        require(allowance >= amount, "Allowance too low for transfer");
+
+        // Transfer EMP tokens from the user to this contract using SafeERC20
+        EMP.safeTransferFrom(msg.sender, address(this), amount);
+
+        // Mint equivalent WEMP tokens to the user
+        _mint(msg.sender, amount);
+
+        // Emit the Wrapped event
+        emit Wrapped(msg.sender, amount);
     }
 
-    /// @notice Mints DOLA tokens by locking BDOLA as collateral
-    /// @param dolaAmount The amount of DOLA to mint
-    function mintDOLA(uint256 dolaAmount) external {
-        uint256 requiredCollateral = calculateCollateral(dolaAmount);
+    /// @notice Unwrap WEMP tokens back into EMP tokens
+    /// @param amount The amount of WEMP tokens to unwrap
+    function unwrap(uint256 amount) external {
+        require(amount > 0, "Amount must be greater than 0");
+
+        // Ensure the contract has enough EMP tokens to perform the unwrap
+        uint256 contractBalance = EMP.balanceOf(address(this));
         require(
-            bdolaToken.balanceOf(msg.sender) >= requiredCollateral,
-            "Insufficient BDOLA balance"
+            contractBalance >= amount,
+            "Not enough EMP tokens in the contract"
         );
 
-        // Transfer BDOLA collateral from the user to the contract
-        bdolaToken.transferFrom(msg.sender, address(this), requiredCollateral);
+        // Burn the WEMP tokens from the user
+        _burn(msg.sender, amount);
 
-        // Mint DOLA tokens to the user
-        _mint(msg.sender, dolaAmount);
+        // Transfer equivalent EMP tokens back to the user using SafeERC20
+        EMP.safeTransfer(msg.sender, amount);
+
+        // Emit the Unwrapped event
+        emit Unwrapped(msg.sender, amount);
     }
 
-    /// @notice Burns DOLA tokens to release collateral
-    /// @param dolaAmount The amount of DOLA to burn
-    function burnDOLA(uint256 dolaAmount) external {
+    /// @notice Owner can withdraw any EMP tokens from the contract (e.g. in emergency)
+    /// @param amount The amount of EMP tokens to withdraw
+    function withdrawEMP(uint256 amount) external {
+        uint256 contractBalance = EMP.balanceOf(address(this));
         require(
-            balanceOf(msg.sender) >= dolaAmount,
-            "Insufficient DOLA balance"
+            contractBalance >= amount,
+            "Not enough EMP tokens in the contract"
         );
 
-        uint256 collateralToReturn = calculateCollateral(dolaAmount);
-
-        // Burn the DOLA tokens from the user
-        _burn(msg.sender, dolaAmount);
-
-        // Transfer equivalent BDOLA collateral back to the user
-        bdolaToken.transfer(msg.sender, collateralToReturn);
+        EMP.safeTransfer(Owner, amount);
     }
 }
